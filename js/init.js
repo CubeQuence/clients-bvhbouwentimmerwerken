ac3656b66885b7cab4db581e984b63d5224bc5b8
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-03-29 09:32:13 +0100

    fixed email and added analytics

--- js/init.js
+++ js/init.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // Contact Form
 function FORMrequest(url, formData, callback) {
-  var xhr = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
 
   xhr.onreadystatechange = function() {
     if (xhr.readyState !== 4) return;
@@ -18,21 +18,71 @@ function FORMrequest(url, formData, callback) {
 }
 
 const contact_form = formElement => {
-  let formData = new FormData(formElement);
+  const formData = new FormData(formElement);
+  let mail_body = null;
+
+  if (!formData.has("phone")) {
+    // Contact
+    mail_body = `
+        <html>
+            <body>
+                <h1>Contact Formulier</h1>
+                <p>
+                    Beste Bart,<br />
+                    Je hebt een bericht gekregen.
+                </p>
+                <p>
+                    Naam: <b>${formData.get("name")}</b><br />
+                    Email: <b><a href="mailto:${formData.get(
+                      "email"
+                    )}">${formData.get("email")}</a></b><br />
+                    Bericht: <b>${formData.get("message")}</b>
+                </p>
+            </body>
+        </html>
+    `;
+  } else {
+    // Afspraak
+    mail_body = `
+        <html>
+            <body>
+                <h1>Afspraak Formulier</h1>
+                <p>
+                    Beste Bart,<br />
+                    Je hebt een afspraak verzoek gekregen.
+                </p>
+                <p>
+                    Naam: <b>${formData.get("name")}</b><br />
+                    Email: <b><a href="mailto:${formData.get(
+                      "email"
+                    )}">${formData.get("email")}</a></b><br />
+                    Telefoonnummer: <b><a href="tel:${formData.get(
+                      "phone"
+                    )}">${formData.get("phone")}</a></b><br />
+                    Adres: <b><a href="https://www.google.com/maps/dir/?api=1&travelmode=driving&destination=${formData.get(
+                      "adress"
+                    )}">${formData.get("adress")}</a></b><br />
+                    Bericht: <b>${formData.get("description")}</b>
+                </p>
+            </body>
+        </html>
+    `;
+  }
 
   formData.append("subject", "Formulier || Website");
-  formData.append("body", "WIP");
+  formData.append("body", mail_body);
 
-  FORMrequest("/mail.php", formData, function(response) {
+  FORMrequest("/mail.php", formData, response => {
     console.log(response);
-    // if (response.success) {
-    //   M.toast({
-    //     html: "Uw bericht is verstuurd!"
-    //   });
-    // } else {
-    //   M.toast({
-    //     html: "Oeps, er ging iets mis!"
-    //   });
-    // }
+    if (response.success) {
+      M.toast({
+        html: "Uw bericht is verstuurd!"
+      });
+      document.querySelector("#form").reset();
+    } else {
+      M.toast({
+        html: "Oeps, er ging iets mis!"
+      });
+    }
   });
 };
